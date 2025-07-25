bcb1acd036447395523c2e1d71d138bf1a437101
KristaDeimos <kristavoitishkina@yandex.ru>
2025-07-25 15:56:06 +0300

    test_wrong_email_registration.py

--- tests/test_wrong_email_registration.py
+++ tests/test_wrong_email_registration.py
@@ -5,3 +5,13 @@ from selenium.webdriver.support import expected_conditions
 from faker import Faker
 from locators import RegistrationLocators as Loc
 from urls import BASE_URL
+
+ def test_registration_invalid_email(self):
+        self.driver.find_element(*Loc.LOGIN_BTN).click()
+        self.driver.find_element(*Loc.NO_ACCOUNT_BTN).click()
+        self.driver.find_element(*Loc.EMAIL_INPUT).send_keys("***")
+        self.driver.find_element(*Loc.CREATE_ACCOUNT_BTN).click()
+
+        error = WebDriverWait(self.driver, 5).until(expected_conditions.visibility_of_element_located(Loc.ERROR_HINT))
+
+        assert "Ошибка" in error.text
