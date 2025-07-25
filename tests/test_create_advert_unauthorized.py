2dc9c21b19faa40e5e75747a3678647156527db1
KristaDeimos <kristavoitishkina@yandex.ru>
2025-07-25 16:05:32 +0300

    test_create_advert_unauthorized.py

--- tests/test_create_advert_unauthorized.py
+++ tests/test_create_advert_unauthorized.py
@@ -6,3 +6,10 @@ from selenium.webdriver.support import expected_conditions
 from locators import RegistrationLocators as RLoc, CreatePostLocators as CLoc
 from urls import BASE_URL
 from tests.test_data import TEST_USER_EMAIL, TEST_USER_PASSWORD
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    driver.get(BASE_URL)
+    yield driver
+    driver.quit()
