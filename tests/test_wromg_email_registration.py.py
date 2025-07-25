0fdcdb1b5768696d1b28bdc4757271711dc8bb99
KristaDeimos <kristavoitishkina@yandex.ru>
2025-07-25 15:54:44 +0300

    test_wrong_email_registration.py.py

--- tests/test_wromg_email_registration.py.py
+++ tests/test_wromg_email_registration.py.py
@@ -1,1 +1,7 @@
-
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from faker import Faker
+from locators import RegistrationLocators as Loc
+from urls import BASE_URL
