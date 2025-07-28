2d76268d0c1ba99973ddc7fcf3455fc0faccc80e
KristaDeimos <kristavoitishkina@yandex.ru>
2025-07-28 22:56:14 +0300

    test_registration.py

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -7,12 +7,7 @@ from locators import RegistrationLocators as Loc
 from urls import BASE_URL
 
 class TestRegistration:
-    def setup_method(self):
-        self.driver = webdriver.Chrome()
-        self.driver.get(BASE_URL)
-        self.driver.quit()
-
-    def test_registration(self):
+    def test_registration(self, driver):
         fake = Faker()
         new_email = fake.email()
         password = fake.password()
